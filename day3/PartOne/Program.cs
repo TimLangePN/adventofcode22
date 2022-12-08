b7874bd905f33e2ae9a3cc6285be1b41e11f4627
Tim Lange <Tim.lange@park-now.com>
2022-12-08 16:46:36 +0100

    day1pt1

---
+++ day3/PartOne/Program.cs
@@ -0,0 +1,38 @@
+﻿var list = File.ReadAllLines(@"C:\Users\tim.lange\Documents\Git\adventofcode22\day3\list.txt");
+
+List<int> points = new List<int>();
+
+foreach (string line in list)
+{
+    points.Add(CompareCharacters(line));
+}
+
+static int CompareCharacters(string line)
+{
+    var first = line.Substring(0, line.Length / 2);
+    var second = line.Replace(first, "");
+
+    foreach (char letter in first)
+    {
+        if (second.Contains(letter))
+        {
+            int Score = GetScore(letter);
+            return Score;
+        }
+    };
+    return 0;
+}
+
+static int GetScore(char letter)
+{
+    if (char.IsUpper(letter) == true)
+    {
+        return(int)letter - 38;
+    }
+    else
+    {
+        return (int)letter - 96;
+    }
+}
+
+Console.WriteLine(points.Sum(x => Convert.ToInt32(x)));
